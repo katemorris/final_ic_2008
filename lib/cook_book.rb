05abc9a688dd0aae51d9e8d120f61105ad94bf2e
Kate Tester <katemorris@gmail.com>
2020-09-23 10:20:25 -0600

    Adds method to return all ingredients in cookbook.

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -7,4 +7,10 @@ class CookBook
   def add_recipe(recipe)
     @recipes << recipe
   end
+
+  def ingredients
+    @recipes.each_with_object([]) do |recipe, array|
+      array << recipe.ingredients.map { |ingredient| ingredient.name }
+    end.flatten.uniq
+  end
 end
