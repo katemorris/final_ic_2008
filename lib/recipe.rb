c4128f9ab991abe489f837a5453d0e4ec524919c
Kate Tester <katemorris@gmail.com>
2020-09-23 09:46:41 -0600

    Adds methods to add ingredients to required and unique lists.

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,8 +1,21 @@
 class Recipe
-  attr_reader :name, :ingredients_required
+  attr_reader :name, :ingredients_required, :ingredients
 
   def initialize(name)
     @name = name
     @ingredients_required = {}
+    @ingredients = []
+  end
+
+  def add_ingredient(ingredient, amount)
+    @ingredients_required[ingredient] ||= 0
+    @ingredients_required[ingredient] += amount
+    add_unique_ingredient(ingredient)
+  end
+
+  def add_unique_ingredient(ingredient)
+    if !@ingredients.include?(ingredient)
+      @ingredients << ingredient
+    end
   end
 end
