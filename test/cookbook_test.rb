39dbaee0ced043bf2684e2cf57e2e16af030bce1
Kate Tester <katemorris@gmail.com>
2020-09-23 10:24:22 -0600

    Adds test to return highest calorie meal.

--- test/cookbook_test.rb
+++ test/cookbook_test.rb
@@ -5,20 +5,58 @@ require './lib/recipe'
 require './lib/cook_book'
 
 class CookbookTest < Minitest::Test
+  def setup
+    @recipe1 = Recipe.new("Mac and Cheese")
+    @recipe2 = Recipe.new("Cheese Burger")
+    @cookbook = CookBook.new
+  end
   def test_it_has_attributes
-    cookbook = CookBook.new
-
-    assert_equal [], cookbook.recipes
+    assert_equal [], @cookbook.recipes
   end
 
   def test_it_can_add_recipes
-    recipe1 = Recipe.new("Mac and Cheese")
-    recipe2 = Recipe.new("Cheese Burger")
-    cookbook = CookBook.new
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
 
-    cookbook.add_recipe(recipe1)
-    cookbook.add_recipe(recipe2)
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
 
-    assert_equal [recipe1, recipe2], cookbook.recipes
+  def test_it_can_return_list_of_ingredients
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+
+    @recipe1.add_ingredient(ingredient1, 2)
+    @recipe1.add_ingredient(ingredient2, 8)
+
+    @recipe2.add_ingredient(ingredient1, 2)
+    @recipe2.add_ingredient(ingredient3, 4)
+    @recipe2.add_ingredient(ingredient4, 1)
+
+    expected = ["Cheese", "Macaroni", "Ground Beef", "Bun"]
+    assert_equal expected, @cookbook.ingredients
+  end
+
+  def test_it_can_return_highest_calorie_recipe
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+
+    ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+    ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+
+    @recipe1.add_ingredient(ingredient1, 2)
+    @recipe1.add_ingredient(ingredient2, 8)
+
+    @recipe2.add_ingredient(ingredient1, 2)
+    @recipe2.add_ingredient(ingredient3, 4)
+    @recipe2.add_ingredient(ingredient4, 1)
+
+    assert_equal @recipe2, @cookbook.highest_calorie_meal
   end
 end
