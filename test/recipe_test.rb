b1161e6e9901571063b568f3b00acd5f91f333d9
Kate Tester <katemorris@gmail.com>
2020-09-23 09:39:26 -0600

    Adds test for adding ingredients

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -14,4 +14,18 @@ class RecipeTest < Minitest::Test
     assert_equal 'Mac and Cheese', @recipe1.name
     assert_empty @recipe1.ingredients_required
   end
+
+  def test_it_can_add_ingredients
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient1, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
+
+    expected = {
+      @ingredient1 => 6,
+      @ingredient2 => 8
+    }
+
+    assert_equal expected, @recipe1.ingredients_required
+    assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
+  end
 end
