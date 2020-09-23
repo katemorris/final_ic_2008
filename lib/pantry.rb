a12f82305e14123ac4aea21c75b9f052a5197adb
Kate Tester <katemorris@gmail.com>
2020-09-23 09:24:47 -0600

    Adds method for stock check.

--- lib/pantry.rb
+++ lib/pantry.rb
@@ -4,4 +4,15 @@ class Pantry
   def initialize
     @stock = {}
   end
+
+  def stock_check(ingredient)
+    found_item = @stock.find do |item|
+      item == ingredient
+    end
+    if found_item.nil?
+      0
+    else
+      found_item.to_a.last
+    end
+  end
 end
