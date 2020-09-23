56496d43ab5f63f78179929c343fd2b83980b45e
Kate Tester <katemorris@gmail.com>
2020-09-23 10:09:31 -0600

    Minor formatting fixes.

--- test/pantry_test.rb
+++ test/pantry_test.rb
@@ -3,10 +3,10 @@ require 'minitest/pride'
 require './lib/ingredient'
 require './lib/pantry'
 
-class IngredientTest < Minitest::Test
+class PantryTest < Minitest::Test
   def setup
     @ingredient1 = Ingredient.new({name: 'Cheese', unit: 'oz', calories: 50})
-    @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 200})
+    @ingredient2 = Ingredient.new({name: 'Macaroni', unit: 'oz', calories: 200})
     @pantry = Pantry.new
   end
 
@@ -16,7 +16,7 @@ class IngredientTest < Minitest::Test
 
   def test_it_can_stock_ingredients
     assert_equal 0, @pantry.stock_check(@ingredient1)
-    
+
     @pantry.restock(@ingredient1, 5)
     @pantry.restock(@ingredient1, 10)
 
--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -31,7 +31,7 @@ class RecipeTest < Minitest::Test
 
   def test_it_can_return_total_calories
     @recipe1.add_ingredient(@ingredient1, 2)
-    @recipe1.add_ingredient(@ingredient2, 4)
+    @recipe1.add_ingredient(@ingredient2, 8)
 
     ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
     ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
@@ -40,7 +40,6 @@ class RecipeTest < Minitest::Test
     recipe2.add_ingredient(@ingredient1, 2)
     recipe2.add_ingredient(ingredient3, 4)
     recipe2.add_ingredient(ingredient4, 1)
-
 
     assert_equal 440, @recipe1.total_calories
     assert_equal 675, recipe2.total_calories
