c437013b2bab5c2f2bdcfaf49fce955c8b59c3cf
Kate Tester <katemorris@gmail.com>
2020-09-23 10:00:19 -0600

    Adds test for total calories.

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -28,4 +28,21 @@ class RecipeTest < Minitest::Test
     assert_equal expected, @recipe1.ingredients_required
     assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
   end
+
+  def test_it_can_return_total_calories
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 4)
+
+    ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+    recipe2 = Recipe.new("Cheese Burger")
+
+    recipe2.add_ingredient(@ingredient1, 2)
+    recipe2.add_ingredient(ingredient3, 4)
+    recipe2.add_ingredient(ingredient4, 1)
+
+
+    assert_equal 440, @recipe1.total_calories
+    assert_equal 675, recipe2.total_calories
+  end
 end
